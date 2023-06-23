5abf673623dea38119aaace4f6ea5278b41ea4e4
walrusOperator <indierevolver@gmail.com>
2023-06-22 21:34:46 -0500

    Updated pom xml and application properties to support mysql. Added script to security config file to test db connection

--- ICS499_Term_Project/src/main/java/com/abc/termproject/config/SecurityConfig.java
+++ ICS499_Term_Project/src/main/java/com/abc/termproject/config/SecurityConfig.java
@@ -11,15 +11,34 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	public Connection connection;
+	
+	
+	//method checks if the database can be connected to
+		private boolean connect() {
+			try {
+				//Each user will need to enter their own username and password for the database
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/EZDB", "root", "Strangerdanger");
+				return true;
+			} catch (Exception ex) {
+				System.out.println("error - database did not connect\n" + ex.getMessage());
+			}
+			return false;
+		}
 
 	// This method is used to store user credentials. After connecting to the DB, we will need to read in the users
 	// into this unless there is another way to do it. For the hard coded users, it works great
 	@Bean
 	public UserDetailsService user() {
+		connect();
+		checkIdAndPassword("test", "password1");
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		UserDetails user = users.username("user").password("user").roles("USER").build();
 		UserDetails admin = users.username("admin").password("admin").roles("USER", "ADMIN").build();
@@ -42,4 +61,24 @@ public class SecurityConfig {
 				.build();
 
 	}
+	
+	private boolean checkIdAndPassword(String userName, String pass) {
+		try {
+			//checks if the username and password are in the database
+			String query = "SELECT * FROM user WHERE username = ? AND pass = ? natural join password";
+			java.sql.PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, userName);
+			statement.setString(2, pass);
+			java.sql.ResultSet resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				System.out.println("User found");
+				return true;
+			}
+		} catch (Exception ex) {
+			System.out.println("error - could not check username and password\n" + ex.getMessage());
+		}
+		System.out.println("Could not find user");
+		return false;
+	}
+	
 }
