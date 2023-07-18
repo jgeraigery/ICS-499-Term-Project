fdb284f1bca3469b364220c405a9ffcf139246cc
Thomas Mullins <85575321+walrusOperator@users.noreply.github.com>
2023-07-18 13:49:06 -0500

    Added methods to verify/cancel deliveries in DB. General house cleaning

--- ICS499_Term_Project/src/main/java/com/abc/termproject/utils/DatabaseUtility.java
+++ ICS499_Term_Project/src/main/java/com/abc/termproject/utils/DatabaseUtility.java
@@ -175,8 +175,9 @@ public class DatabaseUtility {
 		String fullName = "";
 		try { 
 			if(connect()) {
-		        String query = "SELECT * FROM user where userID= " + '"'+id+'"';
+		        String query = "SELECT * FROM user where userID= ?";
 		        PreparedStatement statement = connection.prepareStatement(query);
+		        statement.setInt(1, id);
 		        ResultSet resultSet = statement.executeQuery();
 		        while (resultSet.next()) {
 		            fullName = fullName + resultSet.getString("firstName") + " " + resultSet.getString("lastName");
@@ -199,8 +200,9 @@ public class DatabaseUtility {
 		String fullName = "";
 		try { 
 			if(connect()) {
-		        String query = "SELECT * FROM user where userName= " + '"'+user+'"';
+		        String query = "SELECT * FROM user where userName = ?";
 		        PreparedStatement statement = connection.prepareStatement(query);
+		        statement.setString(1, user);
 		        ResultSet resultSet = statement.executeQuery();
 		        while (resultSet.next()) {
 		            fullName = fullName + resultSet.getString("firstName") + " " + resultSet.getString("lastName");
@@ -223,8 +225,9 @@ public class DatabaseUtility {
 		int id = 0;
 		try { 
 			if(connect()) {
-		        String query = "SELECT userID FROM user where userName= " + '"'+userName+'"';
+		        String query = "SELECT userID FROM user where userName = ? ";
 		        PreparedStatement statement = connection.prepareStatement(query);
+		        statement.setString(1, userName);
 		        ResultSet resultSet = statement.executeQuery();
 		        while (resultSet.next()) {
 		            id = resultSet.getInt("userID");
@@ -286,6 +289,50 @@ public class DatabaseUtility {
 				return true;
 			}} catch (Exception ex) {
 	        System.out.println("error - could not insert into DB see message\n" + ex.getMessage());
+		}
+		return false;
+	}
+	
+	/**
+	 * Method updates status field for a delivery in db to delivered
+	 * @param (int) - deliveryID; unique delivery id number
+	 * @return (Boolean) - return true on successful update, else false
+	 */
+	public Boolean verifyDelivery(int deliveryID) {
+		try {
+			if(connect()) {
+				String insert = "Update deliveries set status = ? where deliveryID = ?";
+		        PreparedStatement stmt = connection.prepareStatement(insert);
+		        stmt.setString(1, "delivered");
+				stmt.setInt(2, deliveryID);
+				int row = stmt.executeUpdate();
+	            System.out.println("Rows affected: " + row);//1
+				connection.close();
+				return true;
+			}} catch (Exception ex) {
+	        System.out.println("error - could not update database\n" + ex.getMessage());
+		}
+		return false;
+	}
+	
+	/**
+	 * Method updates status field for a delivery in db to delivered
+	 * @param (int) - deliveryID; unique delivery id number
+	 * @return (Boolean) - return true on successful update, else false
+	 */
+	public Boolean cancelDelivery(int deliveryID) {
+		try {
+			if(connect()) {
+				String insert = "Update deliveries set status = ? where deliveryID = ?";
+		        PreparedStatement stmt = connection.prepareStatement(insert);
+		        stmt.setString(1, "canceled");
+				stmt.setInt(2, deliveryID);
+				int row = stmt.executeUpdate();
+	            System.out.println("Rows affected: " + row);//1
+				connection.close();
+				return true;
+			}} catch (Exception ex) {
+	        System.out.println("error - could not update database\n" + ex.getMessage());
 		}
 		return false;
 	}
