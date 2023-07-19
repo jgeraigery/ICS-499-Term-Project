9e1cf95462f4f96559dab1c5cb48f96f1dc52fe9
Alexey Malyshkin <amalyshkin00@gmail.com>
2023-07-19 00:21:47 -0500

    Fixed invoice displayer to only display 1 invoice

--- ICS499_Term_Project/src/main/java/com/abc/termproject/utils/UserUtility.java
+++ ICS499_Term_Project/src/main/java/com/abc/termproject/utils/UserUtility.java
@@ -7,6 +7,13 @@ import com.abc.termproject.entity.DateInvoiceNumber;
 import com.abc.termproject.entity.Invoice;
 import com.abc.termproject.entity.InvoiceItem;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class UserUtility {
     
     DatabaseUtility db = new DatabaseUtility();
@@ -35,21 +42,51 @@ public class UserUtility {
         
         String listText = "";
         
-        for (int i = 0; i < userInvoiceList.size(); ++i) {
+        for (int i = 0; i < userInvoiceList.size(); i++) {
             
             String date = userInvoiceList.get(i).getDate();
             int userID = userInvoiceList.get(i).getUserID();
             int invoiceID = userInvoiceList.get(i).getInvoiceID();
             
-            listText += "<li><a href=\"#column2\" onclick=\"   showInvoiceView(" + i + ")   \">User ID: " +
+            listText += "<li><a href=\"#column2\" onclick=\"   " + invoiceDetailsParameterBuilder(date, userID, invoiceID) + "   \">User ID: " +
                 userID + " | Invoice ID: " + invoiceID + " | Date: " + date + "</a></li>";
         }
         
         return listText;
     }
     
+    // Builds the list of all parameters for showInvoiceView() in customer.jsp to display a invoice
+    public String invoiceDetailsParameterBuilder(String date, int userID, int invoiceID) {
+        
+        Invoice myInvoice = db.getInvoice(date, userID, invoiceID);
+        
+        double myInvoiceTotal = myInvoice.getTotal();
+        
+        List<InvoiceItem> myInvoiceItemList = myInvoice.getItemList();
+        
+        int myInvoiceItemListLength = myInvoiceItemList.size();
+        
+        String functionParameters = "showInvoiceView(\'" + date + "\', " + myInvoiceTotal + ", " + myInvoiceItemListLength;
+        
+        for (int j = 0; j < myInvoiceItemListLength; j++) {
+            
+            int productID = myInvoiceItemList.get(j).getProductID();
+            String name = myInvoiceItemList.get(j).getName();
+            String description = myInvoiceItemList.get(j).getDescription();
+            double price = myInvoiceItemList.get(j).getPrice();
+            int quantity = myInvoiceItemList.get(j).getQuantity();
+            
+            functionParameters += ", " + productID + ", \'" + name + "\', \'" + description + "\', " + price + ", " + quantity;
+        }
+        
+        functionParameters += ")";
+        
+        return functionParameters;
+        
+    }
+    
     // Builds the infoboxes for each of the invoices for a user
-    public String invoiceInfoBuilder(List<DateInvoiceNumber> userInvoiceList) {
+    /*public String invoiceInfoBuilder(List<DateInvoiceNumber> userInvoiceList) {
         
         String invoiceText = "";
         
@@ -89,5 +126,5 @@ public class UserUtility {
         }
         
         return invoiceItemsText;
-    }
+    }*/
 }
